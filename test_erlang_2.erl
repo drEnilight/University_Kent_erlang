4616ef434b60bf7af1e9371e92806c1ebef7770a
vladislav romaniuk <vladislav.romaniuk@chisw.com>
2017-03-06 11:14:49 +0200

    commit

--- test_erlang_2.erl
+++ test_erlang_2.erl
@@ -7,27 +7,27 @@ word(Word) ->
     after file:close(Device)
   end.
 
-find_word([], Device, [WH|WT], Acc1, Acc2) ->
-  get_all_lines(Device, [WH|WT], Acc1);
-
-find_word(Line, Device, Word, Acc1, Acc2) ->
-  [LH | LT] = Line,
-  [WH | WT] = Word,
-  io:format("~p~n", [Line]),
-  case Acc2 /= length(Word) of 
-     true -> case LH == WH of
-    		    true -> find_word([LT], Device, [WT], Acc1, Acc2 + 1);
-    		    false -> find_word([LT], Device, Word, Acc1, 0)
-	      end;
-     false -> io:format("~p~n", [{Word, Acc1}]),
-	      get_all_lines(Device, Word, Acc1)
-  end.
-
 get_all_lines(Device, Word) ->
   get_all_lines(Device, Word, 0).
 
 get_all_lines(Device, Word, Acc) ->
   case io:get_line(Device, "") of
       eof  -> [];
-      Line -> find_word(Line, Device, Word, Acc, 0)
+      Line -> Split_string = re:split(Line, " ", [{return, list}]),
+              find_word(Split_string, Device, Word, Acc)
+  end.
+
+find_word([], Device, Word, Acc1) ->
+  get_all_lines(Device, Word, Acc1);
+
+find_word(Line, Device, Word, Acc1) ->
+  [LH | LT] = Line,
+  [WH | WT] = Word.
+  case Acc2 /= length(Word) of
+    true -> case LH == WH of
+      true -> find_word([LT], Device, [WT], Acc1);
+      false -> find_word([LT], Device, Word, Acc1)
+    end;
+    false -> io:format("~p~n", [{Word, Acc1}]),
+             get_all_lines(Device, Word, Acc1)
   end.
